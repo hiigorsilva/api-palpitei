256db77a6ef9db40590fb99c992e3f49502d050c
Higor Code <higorscontato@gmail.com>
2026-04-29 23:30:10 -0300

    fix: rename project and adjust scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "drizzle-kit";
-import { env } from "./src/shared/utils/env";
+import { defineConfig } from 'drizzle-kit'
+import { env } from './src/shared/utils/env'
+import 'dotenv/config'
 
 export default defineConfig({
-	dialect: "postgresql",
-	schema: "src/db/schemas",
-	out: "src/db/migrations",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-});
+  dialect: 'postgresql',
+  schema: 'src/db/schemas',
+  out: 'src/db/migrations',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+})
